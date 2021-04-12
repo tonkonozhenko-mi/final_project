6d7d58c8f972a83a98e606063d6f15fe6ba85dbc
theadmin <theadmin@TeamCity>
2021-04-12 08:36:32 +0000

    TeamCity change in 'ESchool-F' project: runners of 'Build' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -66,6 +66,10 @@ object Build : BuildType({
             name = "docker build"
             scriptContent = "docker build -t front-eschool-mt ."
         }
+        script {
+            name = "docker tag"
+            scriptContent = "docker tag front-eschool-mt:latest 443172575185.dkr.ecr.us-east-2.amazonaws.com/front-eschool-mt:latest"
+        }
     }
 
     triggers {
