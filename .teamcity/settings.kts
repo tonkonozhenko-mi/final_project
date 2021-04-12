e3a2adb0cd1420a3731500e4e6f4b7b3aaaf6a25
theadmin <theadmin@TeamCity>
2021-04-12 10:27:00 +0000

    TeamCity change in 'ESchool-F' project: parameters of 'Build' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -41,7 +41,6 @@ object Build : BuildType({
     params {
         param("AWS_REGION", "--region us-east-2")
         param("ECS_CLUSTER_NAME", "back-eschool-mt")
-        param("ECS_SERVICE_NAME", "api-%ENV%")
         param("ECS_DEPLOY_OPTIONS", "--ignore-warnings --timeout 500")
         param("ECR_IMAGE_NAME", "back-eschool-mt-%ENV%")
         param("ECR_NAME", "443172575185.dkr.ecr.us-east-2.amazonaws.com")
